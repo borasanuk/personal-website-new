b0a032cf86d0a8404bad3bccee1ed858c559f2e5
borasanuk <borasanuk@gmail.com>
2022-10-06 01:49:54 +0300

    remove table test

--- src/components/blog-view/BlogView.tsx
+++ src/components/blog-view/BlogView.tsx
@@ -55,31 +55,6 @@ const BlogView = ({ data, ...props }: BlogPostProps): JSX.Element => {
         )}
       </div>
       <h1 className="mb-3 mt-2">{data.title}</h1>
-      <div className="py-3"></div>
-      <hr />
-      <div className="b-blog-view-table">
-        <div className="b-blog-view-table-item">
-          <h5 className="b-blog-view-table-item-title">Role</h5>
-          <p className="b-blog-view-table-item-content">
-            Web developer, designer
-          </p>
-        </div>
-        <div className="b-blog-view-table-item">
-          <h5 className="b-blog-view-table-item-title">Responsibilites</h5>
-          <p className="b-blog-view-table-item-content">
-            Developing a full-stack app, designing and developing a landing
-            page, creating a visual identity, designing a logo
-          </p>
-        </div>
-        <div className="b-blog-view-table-item">
-          <h5 className="b-blog-view-table-item-title">URL</h5>
-          <p className="b-blog-view-table-item-content">
-            Coming soon!
-          </p>
-        </div>
-      </div>
-      <hr />
-      <div className="py-3"></div>
       <Markdown>{data.content}</Markdown>
     </div>
   );
